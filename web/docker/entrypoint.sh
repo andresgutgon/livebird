08ce7118d507382ebcce013aaefea0e7c8956d43
andresgutgon <andresgutgon@gmail.com>
2021-10-11 13:16:43 +0200

    Init Phoenix app and try to run Docker compose All docker parts looks working but still there is some miss configuration between Nginx and the Phoenix app. They don't see each other.

--- web/docker/entrypoint.sh
+++ web/docker/entrypoint.sh
@@ -9,22 +9,20 @@ do
 done
 
 # Create, migrate, and seed database if it doesn't exist.
-
-#if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
-  #echo "Database $PGDATABASE does not exist. Creating..."
-  #createdb -E UTF8 $PGDATABASE -l en_US.UTF-8 -T template0
-  #mix ecto.migrate
-  #mix run /app/priv/repo/seeds.exs
-  #echo "Database $PGDATABASE created."
-#fi
+if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
+  echo "Database $PGDATABASE does not exist. Creating..."
+  createdb -E UTF8 $PGDATABASE -l en_US.UTF-8 -T template0
+  mix ecto.migrate
+  mix run /app/priv/repo/seeds.exs
+  echo "Database $PGDATABASE created."
+fi
 
 # For debugging use `iex -S mix phx.server`
 # Run only this service wihout Nginx
 # docker-compose run -p 4000:4000 web
 # Access localhost:4000 for example
 echo "::::::::::::::::::::::"
-echo "🚀 Running ${WEB_URL}"
+echo "🚀 Running http://${WEB_URL}"
 echo "::::::::::::::::::::::"
 # exec iex -S mix phx.server
-
-# exec mix phx.server
+ exec mix phx.server
